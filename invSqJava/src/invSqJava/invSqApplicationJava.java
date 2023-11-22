e484ee074d617d7842891647c96a8aa993c01264
PrimusAbOris <guy.ross.rit@gmail.com>
2023-11-22 05:19:18 -0500

    Starting Java example

---
+++ invSqJava/src/invSqJava/invSqApplicationJava.java
@@ -0,0 +1,15 @@
+package invSqJava;
+
+public class invSqApplicationJava {
+
+	public static void main(String[] args) {
+		//Cast args[0] to Integer
+			//If no args[0], prompt
+		
+		//bitshift 1, ssubtract by 0x5f3759df
+			//float?
+		//Print result
+
+	}
+
+}
